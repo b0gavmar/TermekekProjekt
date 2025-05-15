d1881c1ac1cdc81094bc7a30b716b4157368619d
2020b Gavlik Martin <144203996+b0gavmar@users.noreply.github.com>
2025-05-15 16:19:12 +0200

    számítások

---
+++ TermekekProjekt/Controllers/TermekekController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TermekekProjekt.Models;
+
+namespace TermekekProjekt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TermekekController : ControllerBase
+    {
+        private TermekekContext _context = new();
+
+        [HttpGet("/api/purchases/count")]
+        public async Task<IActionResult> GetPurchaseCount()
+        {
+            return Ok(_context.Purchases.Count());
+        }
+
+        [HttpGet("/api/purchases/count/{product_id}")]
+        public async Task<IActionResult> GetPurchaseCountWithProductId([FromRoute]int product_id)
+        {
+            return Ok(_context.Purchases.Where(p => p.ProductId == product_id).Count());
+        }
+
+        [HttpGet("/api/customers/count")]
+        public async Task<IActionResult> GetPurchaseCount([FromQuery] int min_budget)
+        {
+            return Ok(_context.Customers.Where(c => c.Budget>=min_budget).Count());
+        }
+    }
+}
